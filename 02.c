1f724a16d68fa396d86773befdb424b0967f5f91
Bussard <j-senger@gmx.de>
2021-11-24 15:04:28 +0100

    commit.

---
+++ 02.c
@@ -0,0 +1,93 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+int n;
+int m;
+int k;
+
+bool debug = false;
+
+int next(FILE *f) {
+    int i;
+    fscanf(f, "%d", &i);
+    return i;
+}
+
+void skipLine(FILE *filebuf) {
+    fscanf(filebuf, "%*[^\n]\n");
+    if(debug) printf("-");
+}
+
+void skipNumbers(FILE *filebuf, int _n) {
+    if(debug)printf("|%d", _n);
+    for (int i = 0; i < _n; i++) {
+        next(filebuf);
+    }
+    if(debug)printf("|");
+}
+
+FILE *goToA(int _n, bool b) {
+    if (b && debug) { printf("A(%d", _n); }
+    FILE *filebuf = fopen("..\\\\input.txt", "r+");
+    skipLine(filebuf);
+    for (int i = 0; i < _n; i++) {
+        skipLine(filebuf);
+    }
+    if (b && debug) { printf(")"); }
+    return filebuf;
+}
+
+FILE *goToB0(int _n) {
+    _n += 1;
+    if(debug)printf("B(%d", _n);
+    FILE *filebuf = fopen("..\\\\input.txt", "r+");
+    goToA(n - 1, false);
+    skipLine(filebuf);
+    skipLine(filebuf);
+    for (int i = 0; i < _n; i++) {
+        skipLine(filebuf);
+    }
+    if(debug)printf(")");
+    return filebuf;
+}
+
+
+void calculateMatrixProduct() {
+    //initialize file
+    FILE *fp = fopen("..\\\\input.txt", "r+");
+    n = next(fp);
+    k = next(fp);
+    m = next(fp);
+    if(debug)printf("%d,", n);
+    if(debug)printf("%d,", m);
+    if(debug)printf("%d\n", k);
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            if(debug)printf("C{{");
+            int sum = 0;
+            FILE *fa = goToA(i, true);
+            for (int _k = 0; _k < k; _k++) {
+                int na = next(fa);
+                FILE *fb = goToB0(_k);
+                skipNumbers(fb, j);
+                int ba = next(fb);
+                if(debug)printf("%d * %d", na, ba);
+                sum += na * ba;
+            }
+            if(debug)printf("}}");
+            printf("%d ", sum);
+        }
+        printf("\n");
+    }
+
+    //close file
+    fclose(fp);
+}
+
+int main() {
+    calculateMatrixProduct();
+    return 0;
+}
+
+
---
+++ cmake-build-debug/CMakeFiles/A02.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "A02.exe"
+  "A02.exe.manifest"
+  "A02.lib"
+  "A02.pdb"
+  "CMakeFiles/A02.dir/02.c.obj"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/A02.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
